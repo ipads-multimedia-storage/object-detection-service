8fb848036da5278b3ccbdefd2981da144c645f1b
Gusabary <gusabary@126.com>
2020-12-17 14:46:40 +0800

    fix: clean some code

--- detector.py
+++ detector.py
@@ -22,8 +22,6 @@ color_to_bgr = {
     'white': (255, 255, 255),
 }
 
-# size = (640, 480)
-size = (1080, 1440)
 target_color = ('red', 'green', 'blue')
 
 #找出面积最大的轮廓
@@ -40,23 +38,11 @@ def get_max_contour_and_area(contours) :
                 max_contour = c
 
     return max_contour, max_contour_area
-
-def getROI(box):
-    x_min = min(box[0, 0], box[1, 0], box[2, 0], box[3, 0])
-    x_max = max(box[0, 0], box[1, 0], box[2, 0], box[3, 0])
-    y_min = min(box[0, 1], box[1, 1], box[2, 1], box[3, 1])
-    y_max = max(box[0, 1], box[1, 1], box[2, 1], box[3, 1])
-
-    return (x_min, x_max, y_min, y_max)
     
 def detect(image):
-    frame_resize = cv2.resize(image, size, interpolation=cv2.INTER_NEAREST)
-    frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
-    # it seems that this is not necessary
-    #如果检测到某个区域有识别到的物体，则一直检测该区域直到没有为止
-    # if get_roi and not start_pick_up:
-    #     get_roi = False
-    #     frame_gb = getMaskROI(frame_gb, roi, size)      
+    # frame_resize = cv2.resize(image, size, interpolation=cv2.INTER_NEAREST)
+    frame_resize = image.copy()
+    frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)   
     frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # 将图像转换到LAB空间
 
     # the max contour
@@ -83,20 +69,17 @@ def detect(image):
         rect = cv2.minAreaRect(max_contour)
         box = np.int0(cv2.boxPoints(rect))
         
-        roi = getROI(box) #获取roi区域
-        # get_roi = True
-        img_centerx, img_centery = rect[0] # 获取木块中心坐标
-        # img_centerx, img_centery = getCenter(rect, roi, size, square_length)  # 获取木块中心坐标
-        # print("rect:      ({})".format(rect[0]))
-        # print("getCenter: ({}, {})".format(img_centerx, img_centery))
-            
-        # world_x, world_y = convertCoordinate(img_centerx, img_centery, size) #转换为现实世界坐标
+        img_centerx, img_centery = rect[0]  # 获取木块中心坐标
+        angle = rect[2]
+        img_centerx = round(img_centerx, 2)
+        img_centery = round(img_centery, 2)
+        angle = round(angle, 2)
         
         cv2.drawContours(image, [box], -1, color_to_bgr[max_contour_color], 2)
+        cv2.putText(image, '(' + str(img_centerx) + ',' + str(img_centery) + ')',
+            (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color_to_bgr[max_contour_color], 1)  #绘制中心点
         cv2.imwrite("images/tmp.jpg", image)
-        # cv2.putText(image, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
-                # cv2.FONT_HERSHEY_SIMPLEX, 0.5, color_to_bgr[max_contour_color], 1)  #绘制中心点
-        return image, img_centerx, img_centery, rect[2], max_contour_color
-        # return image, world_x, world_y, rect[2], max_contour_color
+        return image, img_centerx, img_centery, angle, max_contour_color
     
     return False
